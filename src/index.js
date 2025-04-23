6ee5732b71c3a2c0f9aa9824a498e217c26cc5a3
Rudakov Dmitry <altbio22@yandex.ru>
2025-04-23 15:48:47 +0700

    test

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import './styles.scss';
 import 'bootstrap';
 import application from './application.js';
 
+alert('Скрипт index.js загружен и работает!');
+
 application();
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ const __dirname = path.dirname(__filename);
 
 export default {
 
-  entry: './src/index.js',
+  //entry: './src/index.js',
+  entry: './indextest.js',
 
   output: { 
     path: path.join(__dirname, "dist"), 
