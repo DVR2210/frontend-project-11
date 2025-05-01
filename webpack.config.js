aa9e3e9d3108da9923d70c9fe3eb93a6e7df1669
Rudakov Dmitry <altbio22@yandex.ru>
2025-05-01 18:34:44 +0700

    final

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,75 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-export default {
+// export default {
 
-  entry: './src/index.js',
+//   entry: './src/index.js',
  
 
-  output: { 
-    path: path.join(__dirname, "dist"), 
-    filename: "main.js"
-  }, 
+//   output: { 
+//     path: path.join(__dirname, "dist"), 
+//     filename: "main.js"
+//   }, 
   
-  module: { 
-    rules: [ 
-       {
-          test: /\.css$/, 
-          use: ["style-loader", "css-loader"] // убрал "sass-loader"
-       },
+//   module: { 
+//     rules: [ 
+//        {
+//           test: /\.css$/, 
+//           use: ["style-loader", "css-loader"] // убрал "sass-loader"
+//        },
 
-       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
+//        {
+//         test: /\.scss$/,
+//         use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+//       },
       
-    ]
-  },
+//     ]
+//   },
   
 
+// };
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export default {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 };
