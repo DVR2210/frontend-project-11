ca60c2f29fc6ea98e942e7ab08aacfa36d103653
Rudakov Dmitry <altbio22@yandex.ru>
2025-05-03 13:33:21 +0700

    final eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,39 +1,3 @@
-// import globals from 'globals'
-// import pluginJs from '@eslint/js'
-// import stylisticJs from '@stylistic/eslint-plugin-js'
-
-// export default [
-//   {
-//     ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
-//   },
-//   {
-//     files: ['**/*.{js,mjs,cjs}'],
-//     languageOptions: {
-//       globals: {
-//         ...globals.browser,
-//         ...globals.node,
-//       },
-//       ecmaVersion: 'latest',
-//       sourceType: 'module',
-//     },
-//   },
-//   pluginJs.configs.recommended,
-//   {
-//     plugins: {
-//       '@stylistic': stylisticJs,
-//     },
-//     rules: {
-//       '@stylistic/semi': ['error', 'always'],
-//       '@stylistic/indent': ['error', 2],
-//       '@stylistic/quotes': ['error', 'single'],
-//       '@stylistic/no-trailing-spaces': 'error',
-//       '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
-//       '@stylistic/eol-last': ['error', 'always'],
-//       '@stylistic/object-curly-spacing': ['error', 'always'],
-//       '@stylistic/arrow-parens': ['error', 'as-needed'],
-//     },
-//   },
-// ]
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import stylisticJs from '@stylistic/eslint-plugin-js'
@@ -53,7 +17,7 @@ export default [
       sourceType: 'module',
     },
     env: {
-      node: true, // Явно указываем поддержку Node.js
+      node: true,
       browser: true,
     },
   },
@@ -64,7 +28,7 @@ export default [
     },
     rules: {
       '@stylistic/semi': ['error', 'always'],
-      '@stylistic/indent': ['error', 2], // Указывает 2 пробела, что может быть причиной других ошибок
+      '@stylistic/indent': ['error', 2], 
       '@stylistic/quotes': ['error', 'single'],
       '@stylistic/no-trailing-spaces': 'error',
       '@stylistic/no-multiple-empty-lines': ['error', { max: 1 }],
